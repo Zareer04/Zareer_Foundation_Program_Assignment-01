09cc394854509e5121956b53fd36e70d6c907194
Zareer04 <kzareer04@gmail.com>
2026-09-02 22:37:29 +0600

    Answer 5 added

--- answer.js
+++ answer.js
@@ -51,3 +51,27 @@ function getcngFare(distance, isNight = false, waitingMinutes = 0) {
 
    return fare;
 }
+
+/*Answer No. 5 */
+const getChaseVerdict = (target, scored, ballsLeft) => {
+   const runsNeeded = target - scored;
+
+   if (runsNeeded <= 0) {
+      return "Won";
+   } else if (ballsLeft <= 0) {
+      return "Lost";
+   }
+
+   const requiredRate = ( runsNeeded / ballsLeft ) * 6;
+
+   let verdict;
+   if (requiredRate <= 6) {
+      verdict = "Comfortable";
+   } else if (requiredRate <= 12) {
+      verdict = "Tough";
+   } else {
+      verdict = "Almost Impossible";
+   }
+
+   return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`
+};
