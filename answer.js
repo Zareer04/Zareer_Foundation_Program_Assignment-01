adf443e1bf4de4abfa55be436492b1abede71ba1
Zareer04 <kzareer04@gmail.com>
2026-09-02 22:55:59 +0600

    All 5 answers added and editted

--- answer.js
+++ answer.js
@@ -2,7 +2,7 @@
 function describeValue(value){
    const type = typeof value;
    const truthiness = value ? "truthy" : "falsy";
-   return `"${type} | ${truthiness}"`;
+   return `${type} | ${truthiness}`;
 }
 
 /*Answer No. 2 */
